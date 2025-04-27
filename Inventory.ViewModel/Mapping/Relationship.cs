f39a71913f068d41fcafc4d95d4fbbf1ff3f5170
Fahrudeen Muhammadh Fayas <muhamadfayas8@gmail.com>
2025-04-27 23:56:11 +0530

    added  Microsoft's official paging logic

---
+++ Inventory.ViewModel/Mapping/Relationship.cs
@@ -0,0 +1,28 @@
+﻿using inventory.models;
+using Inventory.ViewModel.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.ViewModel.Mapping
+{
+    public static class Relationship
+    {
+        public static IEnumerable<CustomerTypeListViewModel> ModelToVM(this IEnumerable<CustomerType> customerType)
+        {
+            List<CustomerTypeListViewModel> List = new List<CustomerTypeListViewModel>();
+            foreach (var ct in customerType)
+            {
+                List.Add(new CustomerTypeListViewModel()
+                {
+                    CustomerTypeId = ct.CustomerTypeId,
+                    CustomerTypeName = ct.CustomerTypeName,
+                    Description = ct.Description
+                });
+            }
+            return List;
+        }
+    }
+}
